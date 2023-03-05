f70ab8714ebc11958640b442bb8ea23a855be810
Herman Shpryhau <herman.shpryhau@gmail.com>
2023-03-05 20:49:17 +0300

    Initial commit

---
+++ src/main/java/com/epam/jmp/springfroundation/JmpSpringFroundationApplication.java
@@ -0,0 +1,13 @@
+package com.epam.jmp.springfroundation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmpSpringFroundationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmpSpringFroundationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epam/jmp/springfroundation/JmpSpringFroundationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.jmp.springfroundation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmpSpringFroundationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
